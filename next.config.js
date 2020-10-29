597b771d1173db8a99c23700f8d102df9afa0d2d
Colin McDonnell <colinmcd@alum.mit.edu>
2020-10-29 01:31:50 -0400

    Added files

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async rewrites() {
+    return [
+      // Rewrite everything else to use `pages/index`
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+    </div>
+  );
+}
+export default App;
